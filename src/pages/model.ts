a4efd12cec365b43bd756e89520de53f64eba484
ilya3dev <ilya3dev@gmail.com>
2024-02-20 00:08:31 +0300

    fix

--- src/pages/model.ts
+++
@@ -1,6 +0,0 @@
-import { createEvent, createStore } from "effector";
-
-export const $name = createStore<number>(0);
-export const setName = createEvent<number>();
-
-$name.on(setName, (_, state) => state);
